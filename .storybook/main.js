98ed741554d4ef77a9d92469cdca8dde82cded3a
Pratik <pratik.satpute@indexnine.com>
2025-02-21 13:39:15 +0530

    Remove Chromatic GitHub Actions workflow and update Storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,10 @@
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-onboarding", // Provides an interactive tutorial for new users to learn Storybook
+    "@storybook/addon-essentials", // Bundle of most commonly used addons (docs, controls, actions, viewport, backgrounds, etc.)
+    "@chromatic-com/storybook", // Enables visual testing and UI review workflows with Chromatic
+    "@storybook/addon-interactions", // Enables testing of component interactions and user flows
   ],
   framework: {
     name: "@storybook/react-vite",
